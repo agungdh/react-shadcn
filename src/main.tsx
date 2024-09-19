e263a4837a5b7083a58ad20f4f8ed779c4cde620
AgungDH <agungdh@live.com>
2024-09-19 09:59:40 +0700

    layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { Dashboard } from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Dashboard />
+
+
+
+
+
   </StrictMode>,
 )
